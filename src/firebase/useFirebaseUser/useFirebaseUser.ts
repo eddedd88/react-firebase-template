4da479e453d8fa305b880916707fe5c39819ac6a
Eduardo Duran <eduardo.durandiaz@gmail.com>
2019-10-30 10:36:01 -0400

    Fix firebase import

--- src/firebase/useFirebaseUser/useFirebaseUser.ts
+++ src/firebase/useFirebaseUser/useFirebaseUser.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import firebase from '..'
+import firebase from '../firebase'
 
 const useFirebaseUser = () => {
   const [user, setUser] = useState<firebase.User | null>()

67d18b1997652af96b7986e0d551348ada59f273
Eduardo Duran <eduardo.durandiaz@gmail.com>
2020-01-04 14:12:19 -0500

    chore: rename confirmation dialog to alert dialog (#11)

---
+++ src/components/DialogAlert/DialogAlert.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent } from 'react'
+// import Dialog, { DialogProps } from '@material-ui/core/Dialog'
+import {
+  Dialog,
+  DialogProps,
+  Button,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText
+} from '@material-ui/core'
+
+type Props = {
+  text: string
+  confirmLabel: string
+  onConfirm: () => void
+  onClose: () => void
+  title?: string
+  cancelLabel?: string
+} & DialogProps
+
+const DialogAlert: FunctionComponent<Props> = props => {
+  const {
+    title,
+    text,
+    confirmLabel,
+    onConfirm,
+    cancelLabel,
+    onClose,
+    children,
+    ...dialogProps
+  } = props
+
+  return (
+    <Dialog {...dialogProps} onClose={onClose}>
+      {title && <DialogTitle>{title}</DialogTitle>}
+
+      <DialogContent>
+        <DialogContentText>{text}</DialogContentText>
+        {children}
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={onClose}>{cancelLabel || 'Cancel'}</Button>
+        <Button variant='contained' onClick={onConfirm} color='primary'>
+          {confirmLabel}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default DialogAlert

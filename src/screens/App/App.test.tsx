a6082ceeddd968ed472163e521322de6a720cbde
Eduardo Duran <eduardo.durandiaz@gmail.com>
2020-01-04 14:07:42 -0500

    chore: dont change default toolbar padding (#10)
    * Import firestore library where we setup firestore

--- src/screens/App/App.test.tsx
+++ src/screens/App/App.test.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 test('renders app title', () => {
   const { getByText } = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MuiThemeProvider theme={createMuiTheme()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MuiThemeProvider>
   )
   expect(getByText('fullstrapp')).toBeInTheDocument()
 })
